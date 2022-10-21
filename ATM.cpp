204ae04a9b38fa08e26a1e2a03d3423a6840f8b8
Abhishek Gupta <83583502+abhishekin1@users.noreply.github.com>
2022-10-21 12:57:11 +0530

    Updated ATM problem

--- ATM.cpp
+++ ATM.cpp
@@ -1,25 +1,15 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
+int main(){
+	int i=5; //101
+	int n=0;
+	int ans=i>>n;
+	cout<<ans;
+}
 
 
-int main() {
-	
-	
-		int withdraw;
-        double total;
-        cout<<"i lv u";
-		// cin>>withdraw;
-        // cin>>total;
-        // if(withdraw%5==0 && total> withdraw+0.50){
-        //     total=(total-withdraw)-0.50;
-        // }
-        // cout.precision(2);
-        // cout<<fixed<<total;
-		
-	
-	
-	
-	return 0;
-}  
+
+
+
+
